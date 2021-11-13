2d851a4fac8ff6f4b05be75a5f3dc67ebb56d12f
MartinPham <MartinPham@users.noreply.github.com>
2021-11-13 13:53:58 +0000

    Manual build bb3d9bb

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const withOffline = require('next-offline')
-
-const nextConfig = {
-  assetPrefix: '/docker-config-generator/',
-  reactStrictMode: true,
-  registerSwPrefix: '/docker-config-generator/'
-}
-
-module.exports = withOffline(nextConfig)
